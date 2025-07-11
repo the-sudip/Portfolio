adb5e1a87a5bb11b705d8cd9c2714a73b4669310
the-sudip <mailofsudipdas@gmail.com>
2025-07-12 02:54:40 +0530

    font added

--- src/app/_components/Navbar.jsx
+++ src/app/_components/Navbar.jsx
@@ -3,68 +3,65 @@ import Button from "./Button";
 import { Lexend } from "next/font/google";
 import { MdOutlineFileDownload } from "react-icons/md";
 
-const lexend = Lexend(
-      {
-        subsets: ["latin"],
-        weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-        style: ["normal"],
-        display: "swap",
-      }
-    )
+
 const Navbar = () => {
-    const downloadHandler = () => {
-        const link = document.createElement("a");
-        link.href = "/resume.pdf";
-        link.download = "Sudip_Das_Resume";
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-    }
-    
+  const downloadHandler = () => {
+    const link = document.createElement("a");
+    link.href = "/resume.pdf";
+    link.download = "Sudip_Das_Resume";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
-    <div className="flex justify-between bg-white py-1">
-      <div className="iconAndTag flex gap-1 ">
+    <div className={` flex justify-between bg-white py-1`}>
+      <div className="iconAndTag flex gap-1 mt-1">
         <div className="icon w-[40px] h-[40px] rounded-full bg-black"></div>
         <h2 className="font-bold text-xl text-black mt-1">Sudip Das</h2>
       </div>
-      <div className="nevigators flex gap-10 text-[#323232] text-[15px] font-semibold mt-1.5">
-        <a
-          href="#skills"
-          className=" hover:text-black hover:cursor-pointer hover:font-medium"
+      <div className="flex gap-10">
+        <div className="nevigators flex gap-10 text-[#323232] text-[15px] font-semibold mt-3">
+          <a
+            href="#skills"
+            className=" hover:text-black hover:cursor-pointer hover:font-bold"
+          >
+            Skills
+          </a>
+          <a
+            href="#projects"
+            className=" hover:text-black hover:cursor-pointer hover:font-bold"
+          >
+            Projects
+          </a>
+          <a
+            href="#experience"
+            className=" hover:text-black hover:cursor-pointer hover:font-bold"
+          >
+            Experience
+          </a>
+          <a
+            href="education"
+            className=" hover:text-black hover:cursor-pointer hover:font-bold"
+          >
+            Education
+          </a>
+          <a
+            href="contactMe"
+            className=" hover:text-black hover:cursor-pointer hover:font-bold"
+          >
+            Contact Me
+          </a>
+        </div>
+
+        <Button
+          type="filled"
+          icon={<MdOutlineFileDownload />}
+          onClick={downloadHandler}
         >
-          Skills
-        </a>
-        <a
-          href="#projects"
-          className=" hover:text-black hover:cursor-pointer hover:font-medium"
-        >
-          Projects
-        </a>
-        <a
-          href="#experience"
-          className=" hover:text-black hover:cursor-pointer hover:font-medium"
-        >
-          Experience
-        </a>
-        <a
-          href="education"
-          className=" hover:text-black hover:cursor-pointer hover:font-medium"
-        >
-          Education
-        </a>
-        <a
-          href="contactMe"
-          className=" hover:text-black hover:cursor-pointer hover:font-medium"
-        >
-          Contact Me
-        </a>
-        <div className="font-normal"><Button type="filled" icon={<MdOutlineFileDownload />} onClick={downloadHandler}>
           Resume
-        </Button></div>
-        
-      
+        </Button>
       </div>
-      
     </div>
   );
 };
