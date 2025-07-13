797dd589a169f4bb71d9c1251f3ed0dfa6e94cb8
the-sudip <mailofsudipdas@gmail.com>
2025-07-13 14:08:32 +0530

    Value animation added

--- src/app/_components/skills/PercentageBar.jsx
+++ src/app/_components/skills/PercentageBar.jsx
@@ -4,6 +4,7 @@ import { useInView } from "react-intersection-observer";
 const PercentageBar = ({ name, percentage }) => {
   console.log("Percentage called");
   // const value = Number(percentage);
+  percentage = Math.floor(percentage)
   const maxLen = 250;
   const { ref, inView, entry } = useInView({
     threshold: 0.5,
@@ -15,7 +16,22 @@ const PercentageBar = ({ name, percentage }) => {
     if (inView) {
       console.log("in view");
       setWidth(percentage);
-      setValue(percentage);
+      const interval = (1000)/20;
+      const increment = Math.floor((percentage) / interval);
+      // const inter = setInterval(() => {
+      //   setValue(prev => prev + increment)
+      // }, interval)
+      let current = 0;
+      const inter = setInterval(() => {
+        current += increment
+        if(current < percentage){
+          setValue(current);
+        }else{
+          setValue(percentage);
+          clearInterval(inter)
+        }
+      })
+      
     } else {
       setWidth(0);
       setValue(0);
@@ -29,11 +45,11 @@ const PercentageBar = ({ name, percentage }) => {
           className={`progressBar w-[250px] h-1 rounded-full bg-gray-300 relative overflow-hidden mr-1`}
         >
           <div
-            className={`progress absolute rounded-full top-0 left-0 bg-black h-full transition-all duration-500 ease-in-out`}
+            className={`progress absolute rounded-full top-0 left-0 bg-black h-full transition-all duration-1000 ease-in-out`}
             style={{ width: `${width}%` }}
           ></div>
         </div>
-        <p className="text-black ">{value}%</p>
+        <p className="text-black">{value}%</p>
       </div>
     </div>
   );
